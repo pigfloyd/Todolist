92862fac48302f9abe46a3f2c49fe61caa16d13d
alan <591634476@qq.com>
2020-11-03 19:29:05 +0800

    update

--- src/model/ItemData.ts
+++ src/model/ItemData.ts
@@ -1,3 +1,4 @@
+import Category from './CateEnum'
 class ItemData {
     id!: number
     categoryId!: number
@@ -5,11 +6,17 @@ class ItemData {
     content!: string
     createTime!: string
 
-    constructor(id: number = -1, categoryId: number = -1, title: string = '', content: string = '') {
+    constructor(id: number = -1, categoryId: Category = -1, title: string = '', content: string = '') {
         this.id = id
         this.categoryId = categoryId
         this.title = title
         this.content = content
-        this.createTime = Date.now().toString()
+        this.createTime = this.toSelfDateStr(Date.now())
+    }
+
+    toSelfDateStr(timeSpan: number): string {
+        let date = new Date(timeSpan)
+        let str = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes()
+        return str
     }
 }
